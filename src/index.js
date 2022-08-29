6af9d3f16767029791ca6d6912497b1c9612e9f6
bkylee <brian.ky.lee@outlook.com>
2022-08-29 17:10:11 -0400

    created page and added a photo

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
+import './style.css';
+import cool from './cool.png';
+
 console.log('hello');
+
+//function ot create page 
+function page(){
+    //get div #content
+    const content = document.querySelector('#content');
+    document.body.appendChild(content);
+
+    //create header 
+    const header = document.createElement('h1');
+    header.textContent = "RESTAURANT NAME";
+    content.appendChild(header);
+
+    //create main text thing for website 
+    const main = document.createElement('div');
+    main.textContent = "This is text about the restaurant and how awesome it is. I love this restaurant.";
+    content.appendChild(main);
+
+    const pic = new Image();
+    pic.src = cool;
+    content.appendChild(pic);
+}
+
+page();
