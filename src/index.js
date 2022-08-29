dfe2198ca1ffcd7e5bb576366364e9c37f9df31c
bkylee <brian.ky.lee@outlook.com>
2022-08-29 18:16:44 -0400

    created header, mainText, picture functions and appended to content

--- src/index.js
+++ src/index.js
@@ -4,25 +4,30 @@ import cool from './cool.png';
 console.log('hello');
 
 //function ot create page 
-function page(){
-    //get div #content
-    const content = document.querySelector('#content');
-    const body = document.body;
-    body.appendChild(content);
-
+function header(){
     //create header 
     const header = document.createElement('h1');
     header.textContent = "RESTAURANT NAME";
-    content.appendChild(header);
 
+    return header;
+};
+
+function mainText() {
     //create main text thing for website 
     const main = document.createElement('div');
     main.textContent = "This is text about the restaurant and how awesome it is. I love this restaurant.";
-    content.appendChild(main);
 
+    return main;
+};
+
+function picture() {
     const pic = new Image();
     pic.src = cool;
-    content.appendChild(pic);
+
+    return pic;
 }
 
-page();
+const content = document.getElementById('content');
+content.appendChild(header());
+content.appendChild(mainText());
+content.appendChild(picture());
